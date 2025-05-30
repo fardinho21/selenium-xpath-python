60bfdd3b6dd038390c6cf468be496fac0d80558c
fardinho21 <fardinhojat8@gmail.com>
2025-05-30 17:41:07 -0400

    updated options

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import time
 
 
 options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--window-size=1920,1080")
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+options.add_argument(f"user-agent={user_agent}")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
