8484ceca873d804d8a74ef06f971a87450743057
fardinho21 <fardinhojat8@gmail.com>
2025-06-04 04:04:04 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from BaseScraper import BaseScraper
+
+
+def main():
+    baseScraper = BaseScraper(url="https://webscraper.io/test-sites/e-commerce/more/phones/touch")
+    pass
+
+
+if __name__ == "__main__":
+    main()
