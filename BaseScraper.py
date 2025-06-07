6f9add14f58e79612f9b341d1e0e029af8a50210
fardinho21 <fardinhojat8@gmail.com>
2025-06-07 14:45:30 -0400

    fixed pagination

--- BaseScraper.py
+++ BaseScraper.py
@@ -5,9 +5,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+#for dynamically loaded website support
+from selenium.webdriver.support import expected_conditions as EXPECTED_CONDS
+from selenium.webdriver.support.ui import WebDriverWait
+
+#for output file types support
 import csv
 import json
 import xml.etree.ElementTree as XML_ETREE
+
+#other imports
+import time
 
 
 '''
@@ -72,8 +80,13 @@ class BaseScraper:
         Scrapes elements specified by an XPath
         and optionally gathers the element's attribute, tag name, or text.
         It also optionally outputs the contents of the data to either a txt, csv, xml, or json file.
+        Compatible with static web pages.
     '''
     def scrapeXPATH(self, xPath:str, attribute:str="", tagName:bool=False, text:bool=False, fileTypeOutput:str=""):
+        if self.scrapeDynamicSite is False:
+            print("Website contains dynamically loaded content.")
+            return
+    
         #scrape elements 
         elements:list[WebElement] = self._scrape(xPath=xPath)
         e : WebElement=None
@@ -94,10 +107,102 @@ class BaseScraper:
             #write to file
             if fileTypeOutput:
                 self.outputToFile(output, fileTypeOutput)
-                
-                
+                            
         except Exception as E:
                 print(E)
+    
+    '''
+        Scrapes dynamically loaded content.
+        loaderType specifies how the dynamic content is loaded (button, scroll, pagination)
+    '''
+    def scrapeXPATH_DynamicLoadButton(self, xPathOfPresenceElement:str="", xPathOfLoadMoreElement:str="", xPathOfElementToScrape:str=""):
+        if not xPathOfPresenceElement:
+            print("xPath of presence element not specified.")
+            return
+        if not xPathOfLoadMoreElement:
+            print("xPath of 'Load More' element not specified.")
+            return 
+        if not xPathOfElementToScrape:
+            print('xPath of element to scrape not specified')
+            return
+        
+        products:list[WebElement]=[]
+        self.driver.get(self.url)
+        WebDriverWait(self.driver, 5).until(EXPECTED_CONDS.presence_of_element_located((By.XPATH, xPathOfPresenceElement)))
+
+        while True:
+            try:
+                load_button = WebDriverWait(self.driver, 5).until(EXPECTED_CONDS.element_to_be_clickable((By.XPATH, xPathOfLoadMoreElement)))
+                self.driver.execute_script("arguments[0].click();", load_button)
+                time.sleep(2)
+
+            except Exception as E:
+                print(E)
+                break
+            
+
+        products=self.driver.find_elements(By.XPATH, xPathOfPresenceElement)
+        for p in products:
+            print(p.find_element(By.XPATH, xPathOfElementToScrape).text)
+            
+    def scrapeXPATH_DynamicScroll(self, xPathOfPresenceElement:str="", xPathOfElementToScrape:str=""):
+        if not xPathOfPresenceElement:
+            print("xPath of presence element not specified.")
+            return
+        if not xPathOfElementToScrape:
+            print("xPath of element to scrape not specified")
+            return
+        
+        products:list[WebElement]=[]
+        self.driver.get(self.url)
+        WebDriverWait(self.driver, 10).until(EXPECTED_CONDS.presence_of_element_located((By.XPATH, xPathOfPresenceElement)))
+        final_height = self.driver.execute_script("return document.body.scrollHeight")
+        
+        while True:
+            try:
+                self.driver.execute_script("return window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+                next_height = self.driver.execute_script("return document.body.scrollHeight")
+                if next_height == final_height:
+                    break
+                final_height=next_height
+            except Exception as E:
+                print(E)
+                break
+        products=self.driver.find_elements(By.XPATH, xPathOfPresenceElement)
+        for p in products:
+            print(p.find_element(By.XPATH, xPathOfElementToScrape).text)
+        
+    def scrapeXPATH_DynamicPagination(self, xPathOfPresenceElement:str="", xPathOfPaginationElement:str="", xPathOfElementToScrape:str="", xPathOther:list[str]=[]):
+        if not xPathOfPresenceElement:
+            print("xPath of presence element not specified.")
+            return
+        if not xPathOfPaginationElement:
+            print("xPath of pagination element not specified.")
+            return
+        if not xPathOfElementToScrape:
+            print("xPath of element to scrape not specified.")
+            return
+        products:list[WebElement]=[]
+        self.driver.get(self.url)
+        WebDriverWait(self.driver, 10).until(EXPECTED_CONDS.presence_of_all_elements_located((By.XPATH, xPathOfPresenceElement)))
+        while True:
+            try:
+                catalog:WebElement=self.driver.find_element(By.XPATH, xPathOfPresenceElement)
+                products=catalog.find_elements(By.XPATH, xPathOfElementToScrape)
+                for p in products:
+                    print(p.text)
+                pagination_next = self.driver.find_element(By.XPATH, xPathOfPaginationElement)
+                self.driver.execute_script("arguments[0].click();", pagination_next)
+                time.sleep(1)
+            except Exception as E:
+                # print(E)
+                print("Pagination End")
+                break
+            
+
+
+    
     '''
         Writes scraped data to an output file of a specified format.
         Supported file types are txt, csv, xml, and json.
@@ -136,4 +241,12 @@ class BaseScraper:
         else:
             print("File type not supported.")
         
+    def printScrapedElements(self, elements:list[WebElement], xPathsOfTextContainingElementsToScrape:list[str]=[]):
+        print(len(xPathsOfTextContainingElementsToScrape))
+        if len(xPathsOfTextContainingElementsToScrape) == 0:
+            print("No paths of text containing elements to scrape provided")
+            return
         
+        for e in elements:
+            for xp in xPathsOfTextContainingElementsToScrape:
+                print(e.find_element(By.XPATH, xp).text)
